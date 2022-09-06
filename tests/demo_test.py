eb10b62e3130bf3a612979c291da81884c78ddcc
Walczak Julia <juliawalczak94@o2.pl>
2022-09-06 15:59:46 +0200

    Add get list of all bookings

--- tests/demo_test.py
+++ tests/demo_test.py
@@ -1,8 +1,11 @@
+import requests
+
 from utilities.configuration import *
 from data.payloads import *
 from data.common_headers import *
 from utilities.methods import *
 
+"Create booking"
 url = get_config()['API']['main_endpoint'] + '/booking'
 body = BookingData.create_booking_payload(booker1)
 
@@ -12,5 +15,12 @@ response_json = create_booking_response.json()
 
 assert create_booking_response.status_code == 200
 assert body == response_json['booking']
+
+booking_id = response_json['bookingid']
+
+"Get list of all bookings"
+create_booking_response2 = requests.get(url)
+response_json2 = create_booking_response2.json()
+print(response_json2)
 
 
