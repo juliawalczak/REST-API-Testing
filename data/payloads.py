fc9f3af8985f57863356bc90c355fd61e01c77f5
Walczak Julia <juliawalczak94@o2.pl>
2022-09-08 11:28:48 +0200

    Add new set of data for update test

--- data/payloads.py
+++ data/payloads.py
@@ -27,3 +27,5 @@ class BookingData(object):
 
 booker1 = BookingData(first_name='Harry', last_name='Potter', total_price=777, deposit_paid=True,
                       check_in='2022-07-07', check_out='2022-07-31', additional_needs='Secret Chamber')
+booker2 = BookingData(first_name='Harry', last_name='Potter', total_price=777, deposit_paid=True,
+                      check_in='2022-07-07', check_out='2022-07-31', additional_needs='Dobby')
